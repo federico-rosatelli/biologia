0dbbdf6c86c606e6c20afae637ffee15bfb463da
federico-rosatelli <frosatelli@gmail.com>
2023-03-10 13:09:10 +0100

    Parsing

--- genbank.py
+++ genbank.py
@@ -1,6 +1,7 @@
 from Bio import SeqIO
 import json
 
+NAME = "file.gbk"
 def parsing(file_name):
 
     record = SeqIO.parse(file_name, "genbank")
@@ -9,7 +10,20 @@ def parsing(file_name):
         json_gene = {}
         json_gene["Name"] = gene.name
         json_gene["Id"] = gene.id
-        json_gene["Seq"] = gene.seq
+        json_gene["Seq"] = str(gene.seq)
+        json_gene["Description"] = gene.description
+        json_gene["Features"] = {}
+        for feature in gene.features:
+            # print(feature.location.start+1)
+            # print(feature.location.end)
+            json_gene["Features"][feature.type] = {}
+            for qual in feature.qualifiers:
+                json_gene["Features"][feature.type][qual] = "".join(feature.qualifiers.get(qual))
+            json_gene["Features"][feature.type]["Location"] = [int(feature.location.start),int(feature.location.end)]
         gene_array.append(json_gene)
+    return gene_array
 
-print(gene_array)
+data = parsing(NAME)
+
+with open("datastruct.json","w") as js:
+    json.dump(data,js,indent=4)
