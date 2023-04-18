0647fd73db31aec219bdc1bdbcd965144791f8f2
federico-rosatelli <frosatelli@gmail.com>
2023-04-18 15:20:39 +0200

    Taxon Finder

--- finder.py
+++ finder.py
@@ -6,7 +6,7 @@ def findTaxon(key):
     client = MongoClient('localhost', 27017)
     db = client["Biologia"]
     collection_taxon = db["taxonomy_data"]
-    rgx = re.compile(f'*{key}*', re.IGNORECASE)
+    rgx = re.compile(f'{key}', re.IGNORECASE)
     info = {"Lineage":rgx}
     dataFind = collection_taxon.find(info)
     for i in dataFind:
