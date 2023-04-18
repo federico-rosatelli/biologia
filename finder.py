2833232bbe178f00de07c87f2aee88eb595340aa
federico-rosatelli <frosatelli@gmail.com>
2023-04-18 15:17:26 +0200

    Taxon Finder

---
+++ finder.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+import re
+
+CLUSTER = "localhost:27017"
+def findTaxon(key):
+    client = MongoClient('localhost', 27017)
+    db = client["Biologia"]
+    collection_taxon = db["taxonomy_data"]
+    rgx = re.compile(f'*{key}*', re.IGNORECASE)
+    info = {"Lineage",rgx}
+    dataFind = collection_taxon.find(info)
+    for i in dataFind:
+        print(i)
+
+findTaxon("Eukaryota")
