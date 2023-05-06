ba71696bd88609a499dd43e9c2efbb6546f0ff8e
federico-rosatelli <frosatelli@gmail.com>
2023-05-06 21:44:42 +0200

    Ma tu che ne sai dei sogni Co-authored-by: Stefano Russo <Samseys@users.noreply.github.com>

--- bombanarchica.py
+++ bombanarchica.py
@@ -1,3 +1,4 @@
+import csv
 import json
 from pymongo import MongoClient
 from Bio import SeqIO, Entrez
@@ -15,6 +16,7 @@ def ncbiSearchTaxon(name:str) ->list:
     handle = Entrez.esearch(db='taxonomy', term=name, rettype='gb', retmode='text', retmax=10000, api_key=key)
     record = Entrez.read(handle, validate=False)
     handle.close()
+    print(record)
     if len(record["IdList"]) == 0:
         raise Exception("List Empty")
     #print(record["IdList"])
@@ -28,13 +30,12 @@ def aglo(name,rank):
     dataResult = collection_data.find(filter)
     return
 
-taxon = ncbiSearchTaxon("chlorella[next level]")
-with open("taxonMeta.json","w") as jsw:
-    json.dump(taxon,jsw,indent=4)
+# taxon = ncbiSearchTaxon("chlorella[next level]")
+# with open("taxonMeta.json","w") as jsw:
+#     json.dump(taxon,jsw,indent=4)
 #print(taxon)
 
-def finder(fn):
-    taxons = []
+def finderTaxon(fn):
     names = []
     rd = open(fn).readlines()
     for i in range(len(rd)):
@@ -46,26 +47,90 @@ def finder(fn):
             print(name,'\t\t',i,len(rd))
             try:
                 taxon = ncbiSearchTaxon(f"{name}[next level]")
+                for tax in taxon:
+                    if not collection_data.find_one({"TaxId":tax["TaxId"]}):
+                        collection_data.insert_one(tax)
             except Exception as e:
                 print(e)
                 pass
             for t in taxon:
                 nameT = t["ScientificName"]
+                print(nameT,'taxon\t\t')
                 try:
                     newTaxon = ncbiSearchTaxon(f"{nameT}[next level]")
-                    taxons = taxons + newTaxon
+                    for ntax in newTaxon:
+                        if not collection_data.find_one({"TaxId":ntax["TaxId"]}):
+                            collection_data.insert_one(ntax)
                 except Exception as e:
                     print(e)
                     continue
-            taxons = taxons+taxon
             names.append(name)
-    return taxons
+    return
 
-taxons = finder('data/databaseCsv/microAlgaeDatabase.csv')
-with open("taxonMetaFull.json","w") as jsw:
-    json.dump(taxons,jsw,indent=4)
+#taxons = finderTaxon('data/databaseCsv/microAlgaeDatabase.csv')
+#db.taxonomy_data.deleteMany({"Lineage":{"$regex":"environmental samples"}})
 
-with open("taxonMetaFull.json") as jsrd:
-    taxons = json.load(jsrd)
-print(len(taxons))
-collection_data.insert_many(taxons,ordered=True)
+
+# allBomb = collection_data.find({},{"ScientificName":1,"_id":0})
+
+# listTuple = [[i["ScientificName"]] for i in allBomb]
+
+# with open('OrganismList.csv', 'w') as csvfile:
+#     writer = csv.writer(csvfile)
+#     writer.writerows(listTuple)
+
+
+new_collection = db["taxonomy_tree"]
+
+def algo():
+    results = collection_data.find({})
+    iter = 0
+    for res in results:
+        print(iter)
+        iter += 1
+        # if new_collection.find_one({"TaxId":res["ParentTaxId"]}):
+        #     dataPush = {
+        #             "TaxId":LineageEx["TaxId"],
+        #             "Rank":LineageEx["Rank"],
+        #             "ScientificName":LineageEx["ScientificName"],
+        #             "SubClasses":[]
+        #         }
+        #     new_collection.insert_one(dataPush)
+        for i in range(1, len(res["LineageEx"])):
+            #for LineageEx in res["LineageEx"]:
+            LineageEx = res["LineageEx"][i]
+            if not new_collection.find_one({"TaxId":LineageEx["TaxId"]}):
+                dataPush = {
+                    "TaxId":LineageEx["TaxId"],
+                    "Rank":LineageEx["Rank"],
+                    "ScientificName":LineageEx["ScientificName"],
+                    "SubClasses":[]
+                }
+                new_collection.insert_one(dataPush)
+            if (i == 1):
+                continue
+            LineageExPrev = res["LineageEx"][i-1]
+
+            if new_collection.find_one({"TaxId":LineageExPrev["TaxId"], "SubClasses":{"$elemMatch":{"TaxId":LineageEx["TaxId"]}}}):
+                continue
+            newDataPush = {
+                    "TaxId":LineageEx["TaxId"],
+                    "Rank":LineageEx["Rank"],
+                    "ScientificName":LineageEx["ScientificName"],
+                    }
+            new_collection.update_one({"TaxId":LineageExPrev["TaxId"]},{"$push":{"SubClasses":newDataPush}})
+
+        if not new_collection.find_one({"TaxId":res["ParentTaxId"], "SubClasses":{"$elemMatch":{"TaxId":res["TaxId"]}}}):
+            newDataPush = {
+                    "TaxId":res["TaxId"],
+                    "Rank":res["Rank"],
+                    "ScientificName":res["ScientificName"],
+                    }
+            new_collection.update_one({"TaxId":res["ParentTaxId"]},{"$push":{"SubClasses":newDataPush}})
+
+
+datas = new_collection.find_one({"ScientificName":"unclassified Chlorella"},{'_id':0})
+
+print(len(datas["SubClasses"]))
+
+
