ebc3d9e3b6078073d0d638e22d0c14ee54595dc0
federico-rosatelli <frosatelli@gmail.com>
2023-05-25 17:11:47 +0200

    ProductFind

--- GOServer/service/database/table.go
+++ GOServer/service/database/table.go
@@ -4,9 +4,11 @@ import (
 	errorM "GOServer/service/ErrManager"
 	str "GOServer/service/structures"
 	"context"
+	"strings"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func (db *appDB) TableOrganism(search string, typeS string, listTaxId []string) ([]str.OrganismTable, errorM.Errors) {
@@ -56,4 +58,59 @@ func (db *appDB) WelcomeMarkDown() (str.Markdown, errorM.Errors) {
 		return mark, errorM.NewError("Can't find Markdown info", errorM.StatusBadRequest)
 	}
 	return mark, nil
+}
+
+func (db *appDB) TableOrganismByProduct(product string) ([]string, errorM.Errors) {
+	var listTaxId []string
+	proj := options.Find().SetProjection(bson.D{{Key: "TaxId", Value: 1}})
+	finder := bson.M{"Products": bson.M{"$elemMatch": bson.M{
+		"$regex": product, "$options": "i"}}}
+	cursor, errM := db.table_basic.Find(context.TODO(), finder, proj)
+	if errM != nil {
+		return listTaxId, errorM.NewError(errM.Error(), errorM.StatusBadRequest)
+	}
+
+	for cursor.Next(context.TODO()) {
+		var tabl str.TableBasic
+		err := cursor.Decode(&tabl)
+		if err != nil {
+			return listTaxId, errorM.NewError("Can't Decode Result", errorM.StatusInternalServerError)
+		}
+		listTaxId = append(listTaxId, tabl.TaxId)
+	}
+
+	return listTaxId, nil
+}
+
+func (db *appDB) TableOrganismByLocation(location string) ([]string, errorM.Errors) {
+	var listTaxId []string
+	proj := options.Find().SetProjection(bson.D{{Key: "GBSeq_feature-table", Value: 1}})
+	finder := bson.M{"GBSeq_feature-table.GBFeature_key": "source",
+		"GBSeq_feature-table.GBFeature_quals.GBQualifier_name": "country",
+		"GBSeq_feature-table.GBFeature_quals.GBQualifier_value": bson.M{
+			"$regex": location, "$options": "i"}}
+	cursor, errM := db.nucleotide_data.Find(context.TODO(), finder, proj)
+	if errM != nil {
+		return listTaxId, errorM.NewError(errM.Error(), errorM.StatusBadRequest)
+	}
+
+	for cursor.Next(context.TODO()) {
+		var nucleo str.Nucleotide
+		err := cursor.Decode(&nucleo)
+		if err != nil {
+			return listTaxId, errorM.NewError("Can't Decode Result", errorM.StatusInternalServerError)
+		}
+		taxId := ""
+		quals := nucleo.GBSeqFeatureTable[0].GBFeatureQuals
+		for i := 0; i < len(quals); i++ {
+			if quals[i].GBQualifierName == "db_xref" {
+				taxId = strings.Split(quals[i].GBQualifierValue, ":")[1]
+			}
+		}
+		if taxId != "" {
+			listTaxId = append(listTaxId, taxId)
+		}
+	}
+
+	return listTaxId, nil
 }
